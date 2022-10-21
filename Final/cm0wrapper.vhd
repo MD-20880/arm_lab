05f111d38206b98a9436250e86a3a244147c15ad
Mingzhang Deng <wu20880@bristol.ac.uk>
2022-10-21 16:19:33 +0100

    Add something

--- Final/cm0wrapper.vhd
+++ Final/cm0wrapper.vhd
@@ -27,14 +27,28 @@ architecture structural of cm0_wrapper is
     component cortexm0
         port(
 
-        )
-    END component cortexm0
+        );
+        
+    END component cortexm0;
 
     component AHB_bridge
         port(
-
-        )
-    END component AHB_bridge
+        -- Clock and Reset -----------------
+        clkm : in std_logic;
+        rstn : in std_logic;
+        -- AHB Master records --------------
+        ahbmi : in ahb_mst_in_type;
+        ahbmo : out ahb_mst_out_type;
+        -- ARM Cortex-M0 AHB-Lite signals -- 
+        HADDR : in std_logic_vector (31 downto 0); -- AHB transaction address
+        HSIZE : in std_logic_vector (2 downto 0); -- AHB size: byte, half-word or word
+        HTRANS : in std_logic_vector (1 downto 0); -- AHB transfer: non-sequential only
+        HWDATA : in std_logic_vector (31 downto 0); -- AHB write-data
+        HWRITE : in std_logic; -- AHB write control
+        HRDATA : out std_logic_vector (31 downto 0); -- AHB read-data
+        HREADY : out std_logic -- AHB stall signal
+        );
+    END component AHB_bridge;
 
 signal HADDR : in std_logic_vector (31 downto 0); -- AHB transaction address
 signal HSIZE : in std_logic_vector (2 downto 0); -- AHB size: byte, half-word or word
@@ -48,9 +62,9 @@ signal HREADY : out std_logic -- AHB stall signal
 
         cortexm0 : cortexm0 port map(
 
-        )
+        );
 
         
         ahb_bridge : AHB_bridge port map(
             
-        )
+        );
--- Final/tb/tb_data_swapper.vhd
+++ Final/tb/tb_data_swapper.vhd
@@ -29,10 +29,16 @@ architecture behavioral of data_swapper_tb is
     constant TEST_FILE_RD :string := "";
     constant C_CLK :time := 10 ns;
     
-    signal clk : std_logic := '0';
-    signal rst : std_logic := '0';
+    signal clk              :std_logic := '0';
+    signal rst              :std_logic := '0';
+    signal data             :std_logic_vector(31 downto 0);
+    signal data_validation  :std_logic_vector(31 downto 0);
+    signal eof              :std_logic := '0';
+    signal checked          :std_logic := '0';
+    signal ready            :std_logic := '0';
 
-    
+    type STD_FILE is file of std_logic_vector(31 downto 0);
+
     signal dmao_t : ahb_dma_out_type;
     signal HRDATA_t : std_logic_vector (31 downto 0);
     -- Test FIle
@@ -47,13 +53,63 @@ architecture behavioral of data_swapper_tb is
             HRDATA => HRDATA_t
         );
 
-    dmao_t.start <= '0';
-    dmao_t.active <= '0';
-    dmao_t.ready <= '0';
-    dmao_t.retry <= '0';
-    dmao_t.mexc <= '0';
-    dmao_t.haddr <= "0000000000";
 
+        ClockGenerator: process
+        begin
+           clk <= '0' after C_CLK, '1' after 2*C_CLK;
+           wait for 2*C_CLK;
+        end process;
+        
+
+        GetData_proc: process
+            variable statrd         :FILE_OPEN_STATUS;
+            variable statwr         :FILE_OPEN_STATUS;
+            variable varstd_data    :std_logic_vector(31 downto 0);
+            variable validation_data :std_logic_vector(31 downto 0);
+         begin
+            data      <= (others => '0');
+            eof       <= '0';
+            wait until rst = '0';
+            file_open(statrd, fptrrd, C_FILE_NAME_RD, read_mode);
+            while (not endfile(fptrrd)) loop
+               wait until clk = '1';
+               read(fptrrd, varstd_data);
+               data <= varstd_data;
+               wait until clk = '1';
+               read(fptrrd, validation_data);
+               data_validation <= validation_data;
+               ready <= '1'; 
+               wait until checked = '1';
+               ready <= '0';
+            end loop;
+            wait until rising_edge(clk);
+            eof       <= '1';
+            file_close(fptrrd);
+            file_close(fptrwr);
+            wait;
+        end process;
+
+         
+
+        testSwapper : process
+            begin
+                dmao_t.start <= '0';
+                dmao_t.active <= '0';
+                dmao_t.ready <= '0';
+                dmao_t.retry <= '0';
+                dmao_t.mexc <= '0';
+                dmao_t.haddr <= "0000000000";
+            while (eof = '0') loop
+                wait until ready = '1';
+                -- TODO Check result
+                checked <='1';
+                wait until ready ='0';
+                checked <= '0';
+            end loop;
+
+
+
+    
     
     
 
