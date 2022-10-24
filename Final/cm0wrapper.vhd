92cdfdce85777334aaf9a8a16a60b914a98a8be2
Mingzhang Deng <wu20880@bristol.ac.uk>
2022-10-24 11:30:44 +0100

    can test

--- Final/cm0wrapper.vhd
+++ Final/cm0wrapper.vhd
@@ -26,6 +26,33 @@ architecture structural of cm0_wrapper is
 
     component cortexm0
         port(
+            -- CLOCK AND RESETS
+            HCLK : in std_logic,
+            HRESETn : in std_logic,
+
+            -- AHB_LITE MASTER PORT
+            HADDR : out std_logic_vector(31 downto 0),
+            HBURST : out std_logic_vector(2 downto 0),
+            HMASTLOCK : out std_logic,
+            HPROT : out std_logic_vector(3 downto 0),
+            HSIZE : out std_logic_vector(2 downto 0),
+            HTRANS : out std_logic_vector(1 downto 0),
+            HWDATA : out std_logic_vector(31 downto 0),
+            HWRITE : out std_logic,
+            HRDATA : in std_logic_vector(31 downto 0),
+            HREADY: in std_logic,
+            HRESP: in std_logic,
+
+            -- MISCELLANEOUS
+            NMI : in std_logic,
+            IRQ : in std_logic_vector(15 downto 0),
+            TXEV :out std_logic,
+            RXEV : in std_logic,
+            LOCKUP: out std_logic,
+            SYSRESETREQ : out std_logic,
+
+            -- POWER MANAGEMENT
+            SLEEPING : out std_logic
 
         );
         
@@ -50,21 +77,56 @@ architecture structural of cm0_wrapper is
         );
     END component AHB_bridge;
 
-signal HADDR : in std_logic_vector (31 downto 0); -- AHB transaction address
-signal HSIZE : in std_logic_vector (2 downto 0); -- AHB size: byte, half-word or word
-signal HTRANS : in std_logic_vector (1 downto 0); -- AHB transfer: non-sequential only
-signal HWDATA : in std_logic_vector (31 downto 0); -- AHB write-data
-signal HWRITE : in std_logic; -- AHB write control
-signal HRDATA : out std_logic_vector (31 downto 0); -- AHB read-data
-signal HREADY : out std_logic -- AHB stall signal
+signal HADDR : std_logic_vector (31 downto 0); -- AHB transaction address
+signal HSIZE : std_logic_vector (2 downto 0); -- AHB size: byte, half-word or word
+signal HTRANS : std_logic_vector (1 downto 0); -- AHB transfer: non-sequential only
+signal HWDATA : std_logic_vector (31 downto 0); -- AHB write-data
+signal HWRITE : std_logic; -- AHB write control
+signal HRDATA : std_logic_vector (31 downto 0); -- AHB read-data
+signal HREADY : std_logic; -- AHB stall signal
 
     begin
 
         cortexm0 : cortexm0 port map(
+            HCLK => clkm,
+            HRESETn => rstn,
+
+            HADDR =>HADDR,
+            HBURST => ahmbo.hburst,
+            HMASTLOCK => ahmbo.lock,
+            HPROT => ahmbo.hprot,
+            HSIZE => ahmbo.hsize,
+            HTRANS => ahmbo.htrans,
+            HWDATA => ahmbo.hwdata,
+            HWRITE => ahmbo.hwrite,
+            HRDATA => ahmbi.hrdata,
+            HREADY => ahmbi.hready,
+            HRESP => ahmbi.hresp,
+
+            NMI => '0',
+            IRQ => "0000000000000000",
+            TXEV => OPEN,
+            RXEV => '0',
+            LOCKUP=>OPEN,
+            SYSRESETREQ => OPEN,
+
+            SLEEPING=>OPEN
 
         );
 
         
         ahb_bridge : AHB_bridge port map(
-            
+            clkm => clkm,
+            rstn => rstn,
+
+            ahbmi => ahbmi,
+            ahbmo => ahbmo,
+
+            HADDR => HADDR,
+            HSIZE => HSIZE,
+            HTRANS => HTRANS,
+            HWDATA => HWDATA,
+            HWRITE => HWRITE,
+            HRDATA => HRDATA,
+            HREADY => HREADY
         );
